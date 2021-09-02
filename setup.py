30ba24ff447e74a63d21aa81b9782b57d01017a8
Theta-Dev <t.testboy@gmail.com>
2021-09-02 22:17:49 +0200

    Bump version: 3.0.1 → 3.0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 
 setuptools.setup(
     name='Spotify-Gender-Ex',
-    version='3.0.1',
+    version='3.0.2',
     author='ThetaDev',
     description='Ein kleines Tool, das die Gendersternchen (z.B. Künstler*innen) aus der Spotify-App für Android entfernt.',
     long_description=README,
