59f84bb55a0464ff8f4f774a8ccc31dac630422b
Theta-Dev <t.testboy@gmail.com>
2022-04-28 02:03:32 +0200

    fix runner script

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,8 +8,11 @@ mkdir -p GenderEx
 
 docker run -v $(pwd)/GenderEx:/GenderEx -v /etc/localtime:/etc/localtime:ro -u 1000:1000 --env-file gex.env --rm thetadev256/spotify-gender-ex:latest
 
-rm -r GenderEx/tmp
-mv GenderEx/output/*.apk ${TARGET_DIR}
+rm -rf GenderEx/tmp
+
+if compgen -G 'GenderEx/output/*.apk' > /dev/null; then
+    mv GenderEx/output/*.apk ${TARGET_DIR}
+fi
 
 # Only keep latest 4 apk files
 ls ${TARGET_DIR}/*.apk -t | tail -n +5 | xargs --no-run-if-empty rm
